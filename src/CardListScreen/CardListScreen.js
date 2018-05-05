124b466cf7d01725f4e50855f69baacaa43d2f6b
Thomas <thomas.grilletta@gmail.com>
2018-05-05 16:47:42 +0900

    Sort japanese series like Bulbapedia

--- src/CardListScreen/CardListScreen.js
+++ src/CardListScreen/CardListScreen.js
@@ -53,8 +53,42 @@ export default class CardListScreen extends React.Component {
   }
 
   refreshCardList(cards, collection, selection) {
+    const typeOrder = {
+      "GRASS": 1,
+      "FIRE": 2,
+      "WATER": 3,
+      "LIGHTNING": 4,
+      "PSYCHIC": 5,
+      "FIGHTING": 6,
+      "COLORLESS": 7,
+      "TRAINER": 8,
+      "ENERGY": 9
+    };
+
+    const rarityOrder = {
+      "COMMON": 1,
+      "UNCOMMON": 2,
+      "RARE": 3,
+      "RARE_HOLO": 3,
+      "SUPER_RARE": 5,
+      "ULTRA_RARE_UNCOMMON": 6,
+      "NONE": 7
+    };
+
     return Object.values(cards)
-      .sort((a, b) => parseInt(a.number) - parseInt(b.number))
+      .sort((a, b) => {
+        const cardNumberDiff = parseInt(a.number) - parseInt(b.number);
+        if (cardNumberDiff != 0) return cardNumberDiff;
+
+        const typeDiff = typeOrder[a.type] - typeOrder[b.type];
+        if (typeDiff != 0) return typeDiff;
+
+        const rarityDiff = rarityOrder[a.rarity] - rarityOrder[b.rarity];
+        if (rarityDiff != 0) return rarityDiff;
+
+        const pokemonNumberDiff = a.pokemonNumber - b.pokemonNumber;
+        if (pokemonNumberDiff != 0) return pokemonNumberDiff;
+      })
       .map(a => ({id: a.id, owned: collection[a.id] != null}))
       .filter(a => {
         if (selection == 'got') return a.owned;
