22fef5b89ac16e3fd5b684d9c82a3e8de41f7dd7
Thomas <thomas.grilletta@gmail.com>
2018-10-18 21:57:33 +0900

    Fix Venusaur GB

--- src/Config/ImageConfig/1998.js
+++ src/Config/ImageConfig/1998.js
@@ -3,6 +3,5 @@ const Year1998 =
   {'DiglettAsobikataPromo.jpg' : require('../../../resources/images/cards/DiglettAsobikataPromo.jpg'),
 'BlankPikachuWizardsPromo24.jpg' : require('../../../resources/images/cards/BlankPikachuWizardsPromo24.jpg'),
 'TouchChangeGaruraParentChildPromo.jpg' : require('../../../resources/images/cards/TouchChangeGaruraParentChildPromo.jpg'),
-'VenusaurWizardsPromo13.jpg' : require('../../../resources/images/cards/VenusaurWizardsPromo13.jpg'),
 'DugtrioPokemonWeb29.jpg' : require('../../../resources/images/cards/DugtrioPokemonWeb29.jpg')};
 export default Year1998
--- src/Config/ImageConfig/GameBoy.js
+++ src/Config/ImageConfig/GameBoy.js
@@ -6,5 +6,6 @@ const GameBoy =
 'LugiaCardGB2Promo.jpg' : require('../../../resources/images/cards/LugiaCardGB2Promo.jpg'),
 'DragoniteCardGBPromo.jpg' : require('../../../resources/images/cards/DragoniteCardGBPromo.jpg'),
 'DarkFearowCardGB2Promo.jpg' : require('../../../resources/images/cards/DarkFearowCardGB2Promo.jpg'),
+'VenusaurWizardsPromo13.jpg' : require('../../../resources/images/cards/VenusaurWizardsPromo13.jpg'),
 'DarkVenusaurBestofGame7.jpg' : require('../../../resources/images/cards/DarkVenusaurBestofGame7.jpg')};
 export default GameBoy
