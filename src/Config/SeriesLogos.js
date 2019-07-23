7e857cee1b1446e909843da688a9efca98142b13
Thomas <thomas.grilletta@gmail.com>
2019-07-23 22:35:00 +0900

    Fix logos

--- src/Config/SeriesLogos.js
+++ src/Config/SeriesLogos.js
@@ -35,5 +35,6 @@ export default SeriesLogos = {
   'SetSymbolVending.png': require('../../resources/images/series/SetSymbolVending.png'),
   'SetSymbolVS.png': require('../../resources/images/series/SetSymbolVS.png'),
   'SetSymbolWind_from_the_Sea.png': require('../../resources/images/series/SetSymbolWind_from_the_Sea.png'),
-  'SetSymbolPokemon_Players.png': require('../../resources/images/series/SetSymbolPokemon_Players.png')
+  'SetSymbolPokemon_Players.png': require('../../resources/images/series/SetSymbolPokemon_Players.png'),
+  'SetSymbolPromo.png': require('../../resources/images/series/SetSymbolPromo.png')
 };
